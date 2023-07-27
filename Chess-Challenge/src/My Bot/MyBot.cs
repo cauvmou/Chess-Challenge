23bf12a1b2b2137a783b9b8d1951c1205186f2f3
Davideko <david.koch.email@gmail.com>
2023-07-28 00:36:17 +0200

    transposition table overhaul + OOP token magic

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,30 +5,19 @@ using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    const byte flagInvalid = 0, flagUpper = 1, flagLower = 2, flagExact = 3;
 
-    enum NodeBound
+    struct TranspositionTableEntry
     {
-        Upper,
-        Lower,
-    }
-
-    class TranspositionTableEntry
-    {
+        public ulong zobristHash;
         public Move move;
-        public int depth;
+        public sbyte depth;
         public double value;
-        public NodeBound? bound;
-
-        public TranspositionTableEntry(Move move, int depth, double value, NodeBound? bound = null)
-        {
-            this.move = move;
-            this.depth = depth;
-            this.value = value;
-            this.bound = bound;
-        }
+        public byte? flag;
     }
 
-    private Dictionary<ulong, TranspositionTableEntry> TranspositionTable = new Dictionary<ulong, TranspositionTableEntry>();
+    private static ulong TpMask = 0x7FFFFF;
+    private TranspositionTableEntry[] TranspositionTable;
 
     private static readonly double[] PieceTypeToValue = { 0, 82, 337, 365, 477, 1025, 0 }; // None, Pawn, Knight, Bishop, Rook, Queen, King
 
@@ -38,56 +27,61 @@ public class MyBot : IChessBot
         // Pawn
         new ulong[]{ 0x0, 0x1919191919191919, 0x5050a0f0f0a0505, 0x202050c0c050202, 0xa0a000000, 0x2fefb0000fbfe02, 0x20505f6f6050502, 0x0 },
         // Knight
-        new ulong[]{ 0xe7ecf1f1f1f1ece7, 0xecf600000000f6ec, 0xf1000507070500f1, 0xf102070a0a0702f1, 0xf100070a0a0700f1, 0xf1020507070502f1, 0xecf600020200f6ec, 0xe7ecf1f1f1f1ece7 },
+        new []{ 0xe7ecf1f1f1f1ece7, 0xecf600000000f6ec, 0xf1000507070500f1, 0xf102070a0a0702f1, 0xf100070a0a0700f1, 0xf1020507070502f1, 0xecf600020200f6ec, 0xe7ecf1f1f1f1ece7 },
         // Bishop
-        new ulong[]{ 0xf6fbfbfbfbfbfbf6, 0xfb000000000000fb, 0xfb000205050200fb, 0xfb020205050202fb, 0xfb000505050500fb, 0xfb050505050505fb, 0xfb020000000002fb, 0xf6fbfbfbfbfbfbf6 },
+        new []{ 0xf6fbfbfbfbfbfbf6, 0xfb000000000000fb, 0xfb000205050200fb, 0xfb020205050202fb, 0xfb000505050500fb, 0xfb050505050505fb, 0xfb020000000002fb, 0xf6fbfbfbfbfbfbf6 },
         // Rook
         new ulong[]{ 0x0, 0x205050505050502, 0xfe000000000000fe, 0xfe000000000000fe, 0xfe000000000000fe, 0xfe000000000000fe, 0xfe000000000000fe, 0x202000000 },
         // Queen
-        new ulong[]{ 0xf6fbfbfefefbfbf6, 0xfb000000000000fb, 0xfb000202020200fb, 0xfe000202020200fe, 0xfe00020202020000, 0xfb000202020202fb, 0xfb000000000200fb, 0xf6fbfbfefefbfbf6 },
+        new []{ 0xf6fbfbfefefbfbf6, 0xfb000000000000fb, 0xfb000202020200fb, 0xfe000202020200fe, 0xfe00020202020000, 0xfb000202020202fb, 0xfb000000000200fb, 0xf6fbfbfefefbfbf6 },
         // King
         new ulong[]{ 0xf1ecece7e7ececf1, 0xf1ecece7e7ececf1, 0xf1ecece7e7ececf1, 0xf1ecece7e7ececf1, 0xf6f1f1ececf1f1f6, 0xfbf6f6f6f6f6f6fb, 0xa0a000000000a0a, 0xa0f050000050f0a },
         // king endgame
-        new ulong[]{ 0xe7ecf1f6f6f1ece7, 0xf1f6fb0000fbf6f1, 0xf1fb0a0f0f0afbf1, 0xf1fb0f14140ffbf1, 0xf1fb0f14140ffbf1, 0xf1fb0a0f0f0afbf1, 0xf1f100000000f1f1, 0xe7f1f1f1f1f1f1e7 }
+        new []{ 0xe7ecf1f6f6f1ece7, 0xf1f6fb0000fbf6f1, 0xf1fb0a0f0f0afbf1, 0xf1fb0f14140ffbf1, 0xf1fb0f14140ffbf1, 0xf1fb0a0f0f0afbf1, 0xf1f100000000f1f1, 0xe7f1f1f1f1f1f1e7 }
     };
     private static double PositionMultiplier = 0.6;
+    private Board board;
+
+    public MyBot()
+    {
+        TranspositionTable = new TranspositionTableEntry[TpMask + 1];
+    }
 
     public Move Think(Board board, Timer timer)
     {
-        System.Console.WriteLine($"TableSize: {TranspositionTable.Count}");
+        this.board = board;
         // depthIncreaseCheck(board);
-        var move = Search(board, double.NegativeInfinity, double.PositiveInfinity, 4, 2, board.IsWhiteToMove).Item1;
+        Console.WriteLine(scoreEval());
+        var move = Search(double.NegativeInfinity, double.PositiveInfinity, 4, 2, board.IsWhiteToMove).Item1;
         return move;
     }
 
     /// <summary> AlphaNegamax </summary>
-    private (Move, double) Search(Board board, double alpha, double beta, int depth, int extensions, bool isWhite)
+    private (Move, double) Search(double alpha, double beta, sbyte depth, int extensions, bool isWhite)
     {
         var alphaCopy = alpha;
-        if (TranspositionTable.ContainsKey(board.ZobristKey))
-        {
-            var entry = TranspositionTable.GetValueOrDefault(board.ZobristKey);
-            if (entry.depth >= depth)
-            {
-                if (entry.bound == null) return (entry.move, entry.value);
-                else if (entry.bound == NodeBound.Lower) alpha = Math.Max(alpha, entry.value);
-                else if (entry.bound == NodeBound.Upper) beta = Math.Min(beta, entry.value);
+        ref TranspositionTableEntry transposition = ref TranspositionTable[board.ZobristKey & TpMask];
 
-                if (alpha >= beta) return (entry.move, entry.value);
-            }
+        if (transposition.zobristHash == board.ZobristKey && transposition.flag != flagInvalid && transposition.depth >= depth)
+        {
+            if (transposition.flag == flagExact) return (transposition.move, transposition.value);
+            else if (transposition.flag == flagLower) alpha = Math.Max(alpha, transposition.value);
+            else if (transposition.flag == flagUpper) beta = Math.Min(beta, transposition.value);
+
+            if (alpha >= beta) return (transposition.move, transposition.value);
         }
 
         var moves = board.GetLegalMoves();
 
         // Depth check
-        if (depth == 0 || board.IsInCheckmate() || board.IsDraw()) return (Move.NullMove, Quiescence(board, alpha, beta, isWhite, extensions));
+        if (depth == 0 || board.IsInCheckmate() || board.IsDraw()) return (Move.NullMove, Quiescence(alpha, beta, isWhite, extensions));
 
         (Move, double) bestValue = (moves[0], double.NegativeInfinity);
 
         foreach (Move legalMove in moves)
         {
             board.MakeMove(legalMove);
-            var score = -Search(board, -beta, -alpha, depth - 1, extensions, !isWhite).Item2;
+            var score = -Search(-beta, -alpha, (sbyte)(depth - 1), extensions, !isWhite).Item2;
             if (score >= beta)
             {
                 bestValue = (legalMove, score);
@@ -103,23 +97,28 @@ public class MyBot : IChessBot
         }
 
         var value = bestValue.Item2;
-        var tableEntry = new TranspositionTableEntry(bestValue.Item1, depth, bestValue.Item2, value <= alphaCopy ? NodeBound.Upper : value >= beta ? NodeBound.Lower : null);
-        if (TranspositionTable.ContainsKey(board.ZobristKey)) TranspositionTable[board.ZobristKey] = tableEntry;
-        else TranspositionTable.Add(board.ZobristKey, tableEntry);
+
+        transposition.zobristHash = board.ZobristKey;
+        transposition.move = bestValue.Item1;
+        transposition.depth = depth;
+        transposition.value = bestValue.Item2;
+        transposition.flag = (value <= alphaCopy)
+                ? flagUpper
+                : value >= beta ? flagLower : flagExact;
 
         return bestValue;
     }
 
-    private double Quiescence(Board board, double alpha, double beta, bool isWhite, int depth)
+    private double Quiescence(double alpha, double beta, bool isWhite, int depth)
     {
-        var best = Evaluate(board) * (isWhite ? 1 : -1);
+        var best = Evaluate() * (isWhite ? 1 : -1);
         if (best >= beta) return beta;
         if (alpha < best) alpha = best;
 
         if (depth > 0) foreach (Move capture in board.GetLegalMoves(true))
             {
                 board.MakeMove(capture);
-                var score = -Quiescence(board, -beta, -alpha, !isWhite, depth - 1);
+                var score = -Quiescence(-beta, -alpha, !isWhite, depth - 1);
                 board.UndoMove(capture);
 
                 if (best >= beta) break;
@@ -130,17 +129,17 @@ public class MyBot : IChessBot
     }
 
     /// <summary> Postive = White is better / Negative = Black is better </summary>
-    private static double Evaluate(Board board)
+    private double Evaluate()
     {
-        if (board.IsInCheckmate())
-        {
-            return board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
-        }
-        else if (board.IsDraw())
-        {
-            return 0;
-        }
+        if (board.IsInCheckmate()) return board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
+        //else if (board.IsDraw()) return drawReluctancy(board)*100;
+        else if (board.IsDraw()) return -scoreEval();
 
+        return scoreEval();
+    }
+
+    private double scoreEval()
+    {
         double materialScore = 0;
         double positionScore = 0;
         foreach (var list in board.GetAllPieceLists())
@@ -150,13 +149,13 @@ public class MyBot : IChessBot
                 double multiplier = piece.IsWhite ? 1 : -1;
                 materialScore += multiplier * PieceTypeToValue[(int)piece.PieceType];
                 var square = new Square(piece.Square.File, (piece.IsWhite ? 7 - piece.Square.Rank : piece.Square.Rank));
-                positionScore += multiplier * ((double)(sbyte)BitConverter.GetBytes(PiecePositionTable[(int)piece.PieceType - 1][square.Rank])[square.File]) * 2.0 * PositionMultiplier;
+                positionScore += multiplier * ((sbyte)BitConverter.GetBytes(PiecePositionTable[(int)piece.PieceType - 1][square.Rank])[square.File]) * 2 * PositionMultiplier;
             }
         }
 
-
         return materialScore + positionScore;
     }
+
 
     // delete this if bot is slow AND stupid 
     // HE WAS!
